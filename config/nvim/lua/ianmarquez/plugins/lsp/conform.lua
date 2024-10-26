bb9c3f5f4f421813cec2bb2fd54220ac953cdd4e
Ian Marquez <regilmarquez123@gmail.com>
2024-10-26 21:11:16 +0800

    Chore: change default text size and corrected javascript spelling in nvim config

--- config/nvim/lua/ianmarquez/plugins/lsp/conform.lua
+++ config/nvim/lua/ianmarquez/plugins/lsp/conform.lua
@@ -6,7 +6,7 @@ return {
 
     conform.setup({
       formatters_by_ft = {
-        javescript = { "prettierd" },
+        javascript = { "prettierd" },
         typescript = { "prettierd" },
         javascriptreact = { "prettierd" },
         typescriptreact = { "prettierd" },
