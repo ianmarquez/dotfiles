f0bb1aee44d095374b606fe53c8df25d991cc542
Ian Marquez <regilmarquez123@gmail.com>
2024-11-21 20:00:21 +0800

    Update: added lazy flag to plugins

--- config/nvim/lua/ianmarquez/plugins/dressing.lua
+++ config/nvim/lua/ianmarquez/plugins/dressing.lua
@@ -1,5 +1,6 @@
 return {
   "stevearc/dressing.nvim",
+  lazy = true,
   events = { "BufReadPre", "BufNewFile" },
   opts = {},
 }
--- config/nvim/lua/ianmarquez/plugins/lsp/mason.lua
+++ config/nvim/lua/ianmarquez/plugins/lsp/mason.lua
@@ -1,6 +1,7 @@
 return {
   "williamboman/mason.nvim",
   cmd = "Mason",
+  lazy = true,
   event = { "BufReadPre", "BufNewFile" },
   dependencies = {
     "williamboman/mason-lspconfig.nvim",
