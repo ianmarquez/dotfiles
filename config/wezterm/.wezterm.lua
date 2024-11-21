9d501cfad7513b3be060df701fb7a8e29bedacf3
Ian Marquez <regilmarquez123@gmail.com>
2024-11-21 22:20:14 +0800

    Update: change to jetbrains mono

--- config/wezterm/.wezterm.lua
+++ config/wezterm/.wezterm.lua
@@ -5,8 +5,12 @@ local wezterm = require 'wezterm'
 local config = wezterm.config_builder()
 
 -- Miscellaneous
--- config.tab_bar_at_bottom = true
 config.automatically_reload_config = true
+config.line_height = 1.05
+
+-- Tab bar
+config.hide_tab_bar_if_only_one_tab = true
+config.use_fancy_tab_bar = false
 
 -- Window
 config.initial_cols = 180
@@ -17,10 +21,13 @@ config.window_padding = {
   top = 10,
   right = 20,
 }
+config.window_background_opacity = 0.9
+config.macos_window_background_blur = 85
+config.window_decorations = "TITLE | RESIZE"
 
 -- Font
-config.font = wezterm.font 'MesloLGL Nerd Font Mono'
-config.font_size = 16
+config.font = wezterm.font 'JetBrains Mono'
+config.font_size = 17
 
 -- Cursor
 config.cursor_blink_ease_in = "Constant"
