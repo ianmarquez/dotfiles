40310207829b6bc946b27f613ddd7e5446432456
Ian Marquez <regilmarquez123@gmail.com>
2025-02-01 02:27:53 +0800

    Update: ad treesitter-context

--- config/nvim/lua/ianmarquez/plugins/nvim-treesitter.lua
+++ config/nvim/lua/ianmarquez/plugins/nvim-treesitter.lua
@@ -1,56 +1,61 @@
 return -- treesitter
 {
-  "nvim-treesitter/nvim-treesitter",
-  dependencies = {
-    "windwp/nvim-ts-autotag"
-  },
-  event = { "BufReadPre", "BufNewFile" },
-  build = ":TSUpdate",
-  -- commit = "364b86e",
-  config = function()
-    require("nvim-ts-autotag").setup({
-      opts = {
-        -- Defaults
-        enable_close = true,          -- Auto close tags
-        enable_rename = true,         -- Auto rename pairs of tags
-        enable_close_on_slash = false -- Auto close on trailing </
-      },
-    })
-    local treesitter = require("nvim-treesitter.configs")
+	"nvim-treesitter/nvim-treesitter",
+	dependencies = {
+		"windwp/nvim-ts-autotag",
+		"nvim-treesitter/nvim-treesitter-context",
+	},
+	event = { "BufReadPre", "BufNewFile" },
+	build = ":TSUpdate",
+	-- commit = "364b86e",
+	config = function()
+		require("treesitter-context").setup({
+			max_lines = 1, -- How many lines the window should span. Values <= 0 mean no limit.
+			trim_scope = "inner", -- Which context lines to discard if `max_lines` is exceeded. Choices: 'inner', 'outer'
+		})
+		require("nvim-ts-autotag").setup({
+			opts = {
+				-- Defaults
+				enable_close = true, -- Auto close tags
+				enable_rename = true, -- Auto rename pairs of tags
+				enable_close_on_slash = false, -- Auto close on trailing </
+			},
+		})
+		local treesitter = require("nvim-treesitter.configs")
 
-    treesitter.setup({
-      -- enable syntax highlighting
-      highlight = {
-        enable = true,
-      },
-      -- enable indentation
-      indent = { enable = true },
-      -- ensure these language parsers are installed
-      ensure_installed = {
-        "json",
-        "javascript",
-        "typescript",
-        "tsx",
-        "yaml",
-        "html",
-        "css",
-        "markdown",
-        "markdown_inline",
-        "svelte",
-        "graphql",
-        "bash",
-        "lua",
-        "vim",
-        "dockerfile",
-        "gitignore",
-        "go",
-        "rust",
-        "zig",
-        "sql",
-        "templ"
-      },
-      -- auto install above language parsers
-      auto_install = true,
-    })
-  end,
+		treesitter.setup({
+			-- enable syntax highlighting
+			highlight = {
+				enable = true,
+			},
+			-- enable indentation
+			indent = { enable = true },
+			-- ensure these language parsers are installed
+			ensure_installed = {
+				"json",
+				"javascript",
+				"typescript",
+				"tsx",
+				"yaml",
+				"html",
+				"css",
+				"markdown",
+				"markdown_inline",
+				"svelte",
+				"graphql",
+				"bash",
+				"lua",
+				"vim",
+				"dockerfile",
+				"gitignore",
+				"go",
+				"rust",
+				"zig",
+				"sql",
+				"templ",
+			},
+			-- auto install above language parsers
+			auto_install = true,
+		})
+	end,
 }
