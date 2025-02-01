0859c3812e43fc3904bb0f744037dd7f5dacfb1d
Ian Marquez <regilmarquez123@gmail.com>
2025-02-01 10:59:00 +0800

    Update: removed webdevicons plugin and used mini.icons instead

--- config/nvim/lua/ianmarquez/plugins/lualine.lua
+++ config/nvim/lua/ianmarquez/plugins/lualine.lua
@@ -1,39 +1,40 @@
 return {
-  "nvim-lualine/lualine.nvim",
-  dependencies = {
-    "nvim-tree/nvim-web-devicons",
-    "meuter/lualine-so-fancy.nvim",
-  },
-  event = { "BufReadPre", "BufNewFile" },
-  lazy = true,
-  opts = {
-    options = {
-      theme = "auto"
-    },
-    sections = {
-      lualine_c = {
-        {
-          "filename",
-          file_status = true,
-          path = 0,
-          symbols = {
-            newFile = "",
-            modified = " ●",
-            readonly = " ",
-          },
-        },
-      },
-      lualine_x = {
-        { "fancy_macro" },
-        {
-          require("lazy.status").updates,
-          cond = require("lazy.status").has_updates,
-          color = { fg = "#FFB86C" },
-        },
-        { "encoding" },
-        { "fileformat" },
-        { "filetype" },
-      },
-    },
-  },
+	"nvim-lualine/lualine.nvim",
+	dependencies = {
+		-- "nvim-tree/nvim-web-devicons",
+		"echasnovski/mini.icons",
+		"meuter/lualine-so-fancy.nvim",
+	},
+	event = { "BufReadPre", "BufNewFile" },
+	lazy = true,
+	opts = {
+		options = {
+			theme = "auto",
+		},
+		sections = {
+			lualine_c = {
+				{
+					"filename",
+					file_status = true,
+					path = 0,
+					symbols = {
+						newFile = "",
+						modified = " ",
+						readonly = " ",
+					},
+				},
+			},
+			lualine_x = {
+				{ "fancy_macro" },
+				{
+					require("lazy.status").updates,
+					cond = require("lazy.status").has_updates,
+					color = { fg = "#FFB86C" },
+				},
+				{ "encoding" },
+				{ "fileformat" },
+				{ "filetype" },
+			},
+		},
+	},
 }
--- config/nvim/lua/ianmarquez/plugins/mini.lua
+++ config/nvim/lua/ianmarquez/plugins/mini.lua
@@ -44,13 +44,13 @@ return {
 			style = "glyph",
 		},
 		lazy = true,
-		specs = { { "nvim-tree/nvim-web-devicons", enabled = false, optional = true } },
-		init = function()
-			package.preload["nvim-web-devicons"] = function()
-				require("mini.icons").mock_nvim_web_devicons()
-				return package.loaded["nvim-web-devicons"]
-			end
-		end,
+		-- specs = { { "nvim-tree/nvim-web-devicons", enabled = false, optional = true } },
+		-- init = function()
+		-- 	package.preload["nvim-web-devicons"] = function()
+		-- 		require("mini.icons").mock_nvim_web_devicons()
+		-- 		return package.loaded["nvim-web-devicons"]
+		-- 	end
+		-- end,
 	},
 	{
 		"echasnovski/mini.tabline",
