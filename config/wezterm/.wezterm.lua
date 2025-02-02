77852742a6903dbc66bea1ed6ccb93c0054a4deb
Ian Marquez <regilmarquez123@gmail.com>
2025-02-02 20:31:16 +0800

    Chore: changed wezterm.lua formatting

--- config/wezterm/.wezterm.lua
+++ config/wezterm/.wezterm.lua
@@ -1,5 +1,5 @@
 -- Pull in the wezterm API
-local wezterm = require 'wezterm'
+local wezterm = require("wezterm")
 
 -- This will hold the configuration.
 local config = wezterm.config_builder()
@@ -19,17 +19,17 @@ config.tab_bar_at_bottom = true
 config.initial_cols = 180
 config.initial_rows = 40
 config.window_padding = {
-  left = 0,
-  bottom = 0,
-  top = 0,
-  right = 0,
+	left = 0,
+	bottom = 0,
+	top = 0,
+	right = 0,
 }
 config.window_background_opacity = 0.9
 config.macos_window_background_blur = 85
 config.window_decorations = "TITLE | RESIZE"
 
 -- Font
-config.font = wezterm.font 'JetBrainsMonoNL Nerd Font Mono'
+config.font = wezterm.font("JetBrainsMonoNL Nerd Font Mono")
 config.font_size = 17
 
 -- Cursor
@@ -38,6 +38,6 @@ config.cursor_blink_ease_out = "Constant"
 config.default_cursor_style = "BlinkingBlock"
 
 -- Color Scheme
-config.color_scheme = 'Catppuccin Mocha'
+config.color_scheme = "Catppuccin Mocha"
 
 return config
