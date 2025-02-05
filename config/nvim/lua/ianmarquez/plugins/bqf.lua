7dc450c2c1f8a3b9004dec963c5412b1440855af
Ian Marquez <regilmarquez123@gmail.com>
2025-02-05 20:44:37 +0800

    Update: added quickfix list window toggle

--- config/nvim/lua/ianmarquez/plugins/bqf.lua
+++ config/nvim/lua/ianmarquez/plugins/bqf.lua
@@ -1,1 +1,27 @@
-return { "kevinhwang91/nvim-bqf", ft = "qf" }
+local M = {}
+
+M.is_open = false
+
+M.toggle_qf = function()
+	if M.is_open == true then
+		vim.cmd([[cclose]])
+	else
+		vim.cmd([[copen]])
+	end
+	M.is_open = not M.is_open
+end
+
+M.config = {
+	"kevinhwang91/nvim-bqf",
+	ft = "qf",
+	keys = {
+		{
+			"<leader>q",
+			M.toggle_qf,
+			desc = "Toggle [q]uickfix list (BQF)",
+			mode = { "n", "v" },
+		},
+	},
+}
+
+return M.config
