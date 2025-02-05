b4e1dd362e8e34c85667bc53d9fd1e9573187c59
Ian Marquez <regilmarquez123@gmail.com>
2025-02-05 14:49:46 +0800

    Chore: added catpuccin supported plugins

--- config/nvim/lua/ianmarquez/plugins/colorscheme.lua
+++ config/nvim/lua/ianmarquez/plugins/colorscheme.lua
@@ -10,12 +10,23 @@ return {
 					blink_cmp = true,
 					gitsigns = true,
 					mason = true,
-					native_lsp = { enabled = true },
+					native_lsp = {
+						enabled = true,
+						underlines = {
+							errors = { "undercurl" },
+							hints = { "undercurl" },
+							warnings = { "undercurl" },
+							information = { "undercurl" },
+						},
+					},
 					nvimtree = true,
+					noice = true,
+					notify = true,
 					symbols_outline = true,
 					telescope = true,
 					treesitter = true,
 					treesitter_context = true,
+					which_key = true,
 				},
 			})
 			-- load the colorscheme here
