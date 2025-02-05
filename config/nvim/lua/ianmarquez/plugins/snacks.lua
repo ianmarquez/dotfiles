35d0060ee1588e0acad0f8659af61d22d1a1d6a2
Ian Marquez <regilmarquez123@gmail.com>
2025-02-05 14:58:11 +0800

    Chore: change row from 10 to 15

--- config/nvim/lua/ianmarquez/plugins/snacks.lua
+++ config/nvim/lua/ianmarquez/plugins/snacks.lua
@@ -13,7 +13,7 @@ return {
 		explorer = {
 			replace_netrw = true,
 		},
-		input = { position = "bottom", win = { style = { row = 10 } } },
+		input = { position = "bottom", win = { style = { row = 15 } } },
 		notifier = { timeout = 1500 },
 		notify = {},
 		picker = {
