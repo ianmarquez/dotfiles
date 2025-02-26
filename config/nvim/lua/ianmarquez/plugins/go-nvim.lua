8872a406a0655fb8bf784395ac17becdebdc7512
Ian Marquez <regilmarquez123@gmail.com>
2025-02-27 00:41:19 +0800

    Update: added go doc plugin for neovim

--- config/nvim/lua/ianmarquez/plugins/go-nvim.lua
+++ config/nvim/lua/ianmarquez/plugins/go-nvim.lua
@@ -1,26 +1,67 @@
 return {
-	"ray-x/go.nvim",
-	dependencies = { -- optional packages
-		"ray-x/guihua.lua",
-		"neovim/nvim-lspconfig",
-		"nvim-treesitter/nvim-treesitter",
-	},
-	build = ':lua require("go.install").update_all_sync()', -- if you need to install/update all binaries
-	event = { "BufReadPre *.go", "BufNewFile *.go", "CmdlineEnter *.go" },
-	ft = { "go", "gomod" },
-	opts = {},
-	keys = {
-		{
-			"<leader>gta",
-			":GoAddTag<CR>",
-			desc = "Add go struct tags (Go)",
-			mode = { "n" },
+	{
+		"fredrikaverpil/godoc.nvim",
+		version = "*",
+		dependencies = {
+			{ "folke/snacks.nvim" }, -- optional
+			{
+				"nvim-treesitter/nvim-treesitter",
+				opts = {
+					ensure_installed = { "go" },
+				},
+			},
 		},
-		{
-			"<leader>gtr",
-			":GoRmTag<CR>",
-			desc = "Remove go struct tags (Go)",
-			mode = { "n" },
+		event = { "BufReadPre *.go", "BufNewFile *.go", "CmdlineEnter *.go" },
+		ft = { "go", "gomod" },
+		build = "go install github.com/lotusirous/gostdsym/stdsym@latest", -- optional
+		cmd = { "GoDoc" }, -- optional
+		opts = {
+			picker = {
+				type = "snacks",
+				snacks = {
+					layout = "vertical",
+				},
+			},
+		},
+		keys = {
+			{
+				"<leader>gtd",
+				function()
+					vim.cmd([[GoDoc]])
+				end,
+				desc = "View go documentation",
+				mode = { "n" },
+			},
+		},
+	},
+	{
+		"ray-x/go.nvim",
+		dependencies = { -- optional packages
+			"ray-x/guihua.lua",
+			"neovim/nvim-lspconfig",
+			"nvim-treesitter/nvim-treesitter",
+		},
+		build = ':lua require("go.install").update_all_sync()', -- if you need to install/update all binaries
+		event = { "BufReadPre *.go", "BufNewFile *.go", "CmdlineEnter *.go" },
+		ft = { "go", "gomod" },
+		opts = {},
+		keys = {
+			{
+				"<leader>gta",
+				function()
+					vim.cmd([[GoAddTag]])
+				end,
+				desc = "Add go struct tags (Go)",
+				mode = { "n" },
+			},
+			{
+				"<leader>gtr",
+				function()
+					vim.cmd([[GoRmTag]])
+				end,
+				desc = "Remove go struct tags (Go)",
+				mode = { "n" },
+			},
 		},
 	},
 }
