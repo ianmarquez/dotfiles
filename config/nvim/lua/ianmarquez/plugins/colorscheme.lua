6d2e4f04a5d49754f3e7231981e7d04f1c7ced15
Ian Marquez <regilmarquez123@gmail.com>
2025-05-15 22:37:41 +0800

    Update: add rospine color scheme

--- config/nvim/lua/ianmarquez/plugins/colorscheme.lua
+++ config/nvim/lua/ianmarquez/plugins/colorscheme.lua
@@ -1,4 +1,11 @@
 return {
+	-- {
+	-- 	"rose-pine/neovim",
+	-- 	name = "rose-pine",
+	-- 	config = function()
+	-- 		vim.cmd("colorscheme rose-pine")
+	-- 	end,
+	-- },
 	{
 		"catppuccin/nvim",
 		name = "catppuccin",
