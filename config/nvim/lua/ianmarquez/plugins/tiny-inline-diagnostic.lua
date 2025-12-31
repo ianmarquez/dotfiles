72e593418bb17b91992d0cd3a8d9350e8e38b59d
Ian Marquez <regilmarquez123@gmail.com>
2025-12-31 13:59:35 +0800

    Update: update mouseless config

--- config/nvim/lua/ianmarquez/plugins/tiny-inline-diagnostic.lua
+++ config/nvim/lua/ianmarquez/plugins/tiny-inline-diagnostic.lua
@@ -1,26 +1,28 @@
 return {
-	"rachartier/tiny-inline-diagnostic.nvim",
-	event = "VeryLazy",
-	priority = 1000,
-	config = function()
-		require("tiny-inline-diagnostic").setup({
-			preset = "powerline",
-			options = {
-				multilines = {
-					enabled = true,
+	{
+		"rachartier/tiny-inline-diagnostic.nvim",
+		event = "VeryLazy",
+		priority = 1000,
+		config = function()
+			require("tiny-inline-diagnostic").setup({
+				preset = "powerline",
+				options = {
+					multilines = {
+						enabled = true,
+					},
 				},
+			})
+			vim.diagnostic.config({ virtual_text = false }) -- Disable Neovim's default virtual text diagnostics
+		end,
+		keys = {
+			{
+				"<leader>dk",
+				function()
+					require("tiny-inline-diagnostic").toggle()
+				end,
+				desc = "Toggle inline diagnostics (Diagnostics)",
+				mode = { "n", "v" },
 			},
-		})
-		vim.diagnostic.config({ virtual_text = false }) -- Disable Neovim's default virtual text diagnostics
-	end,
-	keys = {
-		{
-			"<leader>dk",
-			function()
-				require("tiny-inline-diagnostic").toggle()
-			end,
-			desc = "Toggle inline diagnostics (Diagnostics)",
-			mode = { "n", "v" },
 		},
 	},
 }
